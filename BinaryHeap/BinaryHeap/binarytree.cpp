fffef961c1fba88025ebaa37c7f5b92830b62f37
Unknown <rherediag@ulasalle.edu.pe>
2018-10-27 03:44:06 -0500

    BH commit
    first commit

---
+++ BinaryHeap/BinaryHeap/binarytree.cpp
@@ -0,0 +1,88 @@
+
+#include "binarytree.h"
+#include <algorithm>
+#include <iostream>
+
+binarytree::binarytree()
+{
+	_size = tree.size();
+}
+
+binarytree::~binarytree()
+{
+	//dtor
+}
+int binarytree::lleaf(int pos_parent) {
+	return (2 * pos_parent) + 1;
+}
+
+int binarytree::rleaf(int pos_parent) {
+	return (2 * pos_parent) + 2;
+}
+int binarytree::parent(int pos_leaf) {
+	return (pos_leaf - 1) / 2;
+
+}
+void binarytree::down(int x) {
+	//temporal que guarda la posicion
+	int temp;
+	//Revisa si el nodo solo tiene una hoja
+	if (rleaf(x) <= _size) {
+		//Compara las hojas y elije el mayor
+		if (tree[lleaf(x)] > tree[rleaf(x)]) {
+			//Si el padre es mayor se intercambian
+			if (tree[x] < tree[lleaf(x)]) {
+				//guarda la posicion de la hoja
+				temp = lleaf(x);
+				//intercambia la hoja por el nodo
+				std::swap(tree[x], tree[lleaf(x)]);
+				//llama a la funcion con la nueva posicion del nodo
+				down(tree[temp]);
+			}
+			else return;
+		}
+		if (tree[lleaf(x)] < tree[rleaf(x)]) {
+			if (tree[x] < tree[rleaf(x)]) {
+				temp = rleaf(x);
+				std::swap(tree[x], tree[rleaf(x)]);
+				down(tree[temp]);
+			}
+			else return;
+		}	
+	}
+	else {
+		if (tree[x] < tree[lleaf(x)]) {
+			//guarda la posicion de la hoja
+			temp = lleaf(x);
+			//intercambia la hoja por el nodo
+			std::swap(tree[x], tree[lleaf(x)]);
+			//llama a la funcion con la nueva posicion del nodo
+			return;
+		}
+		else return; 
+	}
+}
+void binarytree::up(int x) {
+	int temp;
+	if (tree[x] > tree[parent(x)]) {
+		temp = tree[parent(x)];
+		std::swap(tree[x], tree[parent(x)]);
+		up(tree[temp]);
+		return;
+	}
+	else return;
+}
+void binarytree::push(int x) {
+	tree.push_back(x);
+	up(tree.size() - 1);
+}
+void binarytree::_delete(int x) {
+	if (_size = 0)
+		return;
+	std::swap(tree[0], tree[_size - 1]);
+	tree.resize(_size - 1);
+	_size -= 1;
+	down(0);
+	
+	return;
+}
---
+++ BinaryHeap/BinaryHeap/binarytree.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+
+class binarytree
+{
+public:
+	int _size;
+	std::vector<int>  tree;
+	binarytree();
+	int lleaf(int);
+	int rleaf(int);
+	int parent(int);
+	void push(int);
+	void up(int);
+	void down(int);
+	void _delete(int);
+	virtual ~binarytree();
+
+protected:
+
+private:
+};
+
+
